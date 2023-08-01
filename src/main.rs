0121fcb9a258b9704577549fd81f0784e68dfc6a
chonkyboy <boskeroni@gmail.com>
2023-08-01 16:17:44 +0100

    converted the Vec<Vec<u8>> into Vec<u8>. makes it quicker by approx 10ms

--- src/main.rs
+++ src/main.rs
@@ -3,33 +3,35 @@ use std::env;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
-fn possiblitys(board: &Vec<Vec<u8>>, row: usize, col: usize) -> Vec<u8> {
-    let square_row = row/3*3;
-    let square_col = col/3*3;
+fn possibles(board: &Vec<u8>, i: usize) -> Vec<u8> {
+    let row_start = i/9*9;
+    let col_start = i%9;
+    let square_row = i/27*27;
+    let square_col = ((i%9)/3)*3;
     let mut all = vec![1, 2, 3, 4, 5, 6, 7, 8, 9];
-    for i in 0..9 {
-        all.retain(|d| *d != board[row][i]); // same col
-        all.retain(|d| *d != board[i][col]); // same row
-        all.retain(|d| *d != board[square_row + i%3][square_col + i/3]); // same square
+    for j in 0..9 {
+        all.retain(|d| *d != board[col_start+j*9]); // same col
+        all.retain(|d| *d != board[row_start+j]); // same row
+        all.retain(|d| *d != board[square_row+(j/3)*9 + square_col+j%3]); // same square
     }
     all
 }
 
-fn recursive_solve(solving: &mut Vec<Vec<u8>>, i: usize, core: &Vec<Vec<u8>>,) -> bool {
+fn recursive_solve(solving: &mut Vec<u8>, i: usize, core: &Vec<u8>,) -> bool {
     if i == 81 {
         return true;
     }
-    if core[i/9][i%9] != 0 {
+    if core[i] != 0 {
         return recursive_solve(solving, i+1, core);
     }
-    let possibles = possiblitys(solving, i/9, i%9);
+    let possibles = possibles(solving, i);
     for trial in possibles {
-        solving[i/9][i%9] = trial;
+        solving[i] = trial;
         if recursive_solve(solving, i+1, core) {
             return true;
         }
     }
-    solving[i/9][i%9] = 0;
+    solving[i] = 0;
     return false;
 }
 
@@ -44,7 +46,7 @@ fn main() {
     };
 
     let reader = BufReader::new(file);
-    let core: Vec<Vec<u8>> = reader.lines().map(|line| {
+    let core: Vec<u8> = reader.lines().map(|line| {
         let line = line.unwrap();
         if line.len() != 9 {
             panic!("invalid row length");
@@ -53,24 +55,24 @@ fn main() {
             match c.to_digit(10) {
                 Some(d) => d as u8,
                 None => panic!("invalid character"),
-            }).collect()
-    }).collect();
+            }).collect::<Vec<u8>>()
+    }).flatten().collect();
 
-    if core.len() != 9 {
+    if core.len() != 81 {
         panic!("invalid number of rows provided in file");
     }
 
-    let mut solved: Vec<Vec<u8>> = core.clone();
+    let mut solved: Vec<u8> = core.clone();
     recursive_solve(&mut solved, 0, &core);
-    for (i, line) in solved.iter().enumerate() {
-        for (j, digit) in line.iter().enumerate() {
-            print!("{digit}");
-            if j % 3 == 2 && j != 8 {
-                print!("|")
-            }
+    for (i, digit) in solved.iter().enumerate() {
+        print!("{digit}");
+        if i % 3 == 2 && i % 9 != 8 {
+            print!("|");
         }
-        println!("");
-        if i % 3 == 2 && i != 8 {
+        if i % 9 == 8 {
+            println!("");
+        }
+        if i % 27 == 26 && i != 80 {
             println!("---+---+---");
         }
     }
